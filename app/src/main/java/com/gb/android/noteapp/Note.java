75a77d7ad4845c33d65bb74701e89ab2d0a1e75e
1406201502102017 <92741096+1406201502102017@users.noreply.github.com>
2022-06-15 23:36:15 +0600

    Update Note.java

--- app/src/main/java/com/gb/android/noteapp/Note.java
+++ app/src/main/java/com/gb/android/noteapp/Note.java
@@ -1,68 +1,104 @@
 package com.gb.android.noteapp;
 
-public class Note {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    private long ID;
+import java.util.Date;
+
+public class Note implements Parcelable {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
     private String title;
-    private String content;
-    private String date;
-    private String time;
+    private String description;
+    private int picture;
+    private boolean done;
+    private Date date;
 
-    Note() {}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    Note(String title, String content, String date, String time) {
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
 
-        this.title = title;
-        this.content = content;
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void setDate(Date date) {
         this.date = date;
-        this.time = time;
     }
 
-    public long getID() {
-        return ID;
+    protected Note(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        picture = in.readInt();
+        done = in.readByte() != 0;
     }
 
-    public void setID(long ID) {
-        this.ID = ID;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeInt(picture);
+        dest.writeByte((byte) (done ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Note(String title, String description, int picture, boolean done, Date date){
+        this.title = title;
+        this.description=description;
+        this.picture=picture;
+        this.done = done;
+        this.date = date;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getDescription() {
+        return description;
     }
 
-    public String getContent() {
-        return content;
+    public int getPicture() {
+        return picture;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    Note(long ID, String title, String content, String date, String time) {
-        this.ID = ID;
-        this.title = title;
-        this.content = content;
-        this.date = date;
-        this.time = time;
+    public boolean isDone() {
+        return done;
     }
 }
